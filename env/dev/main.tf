0cc429a0db52a2f3681b40fa4a146226590226b4
CeciliaCode <180226090+CeciliaCode@users.noreply.github.com>
2024-10-21 14:59:49 -0600

    Extra resource

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -24,7 +24,7 @@ module "dev_vm" {
   SUBNET_NAME = var.SUBNET_NAME
 }
 
-resource "azurerm_resource_group" "monomapceci6" {
-  name = "monomapceci6"
+resource "azurerm_resource_group" "monomapceci7" {
+  name = "monomapceci7"
   location = "eastus2"
 }
